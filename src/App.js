4402157455eef16955d3498632bb379d442e9bc9
paras9sid <sidharthjainp9@gmail.com>
2023-03-24 16:37:56 +0530

    first component created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>react udemy project started</h1>
+      <p>this is visible</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
