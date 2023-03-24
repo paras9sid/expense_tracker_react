13d5ef7a98c57e1bd509587d467c23d2e11b1482
paras9sid <sidharthjainp9@gmail.com>
2023-03-24 17:45:07 +0530

    Outputting Dynamic data & jsx expressions working

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  //adding js code below -- no relation to react
+  const expenseDate = new Date(2023, 2, 24);
+  const expenseTitle = "House to buy";
+  const expenseAmount = 1.5;
   return (
     <div className="expense-item">
-      <div>25th March 2023</div>
+      {/* <div>24th March 2023</div> */}
+      {/* <div>{expenseDate}</div> will throw error - date object cant be used like this*/}
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>House to buy</h2>
-        <div className="expense-item__price ">Rs.1.5 Cr.</div>
+        {/* <h2>House to buy</h2> */}
+        {/* hard coded data example below  */}
+        {/* {Math.random()} -- will add randon muber differenct everytime*/}
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price ">Rs.{expenseAmount} Cr.</div>
       </div>
     </div>
   );
