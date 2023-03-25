d49d2e538aab027d7bb65de83843d1b482e33840
paras9sid <sidharthjainp9@gmail.com>
2023-03-25 21:57:22 +0530

    expenses component created and app com simplified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-function App(props) {
-  //dummy data hard coded -rendered via props
+function App() {
   const expenses = [
     {
       id: 1,
@@ -33,28 +32,7 @@ function App(props) {
   return (
     <div className="App">
       <h1>Expense Tracking project</h1>
-      {/* expense value set dynamiccaly by props below inside single curly braces */}
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      />
+
+      <ExpenseItem
+        title={props.items[1].title}
+        date={props.items[1].date}
+        amount={props.items[1].amount}
+      />
+
+      <ExpenseItem
+        title={props.items[2].title}
+        date={props.items[2].date}
+        amount={props.items[2].amount}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        date={props.items[3].date}
+        amount={props.items[3].amount}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
