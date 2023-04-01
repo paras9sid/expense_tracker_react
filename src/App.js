33fe04bddf2c0441cd572c69355405a0e711d474
paras9sid <sidharthjainp9@gmail.com>
2023-04-01 20:36:38 +0530

    Revision

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
+// import React from "react";
+
+// import Expenses from "./components/Expenses/Expenses";
+
+// // function App() {
+// //using arrow functions - alternative function syntax
+// const App = () => {
+//   const expenses = [
+//     {
+//       id: 1,
+//       date: new Date(2025, 5, 3),
+//       title: "house to buy",
+//       amount: 1.5,
+//     },
+//     {
+//       id: 2,
+//       date: new Date(2023, 6, 24),
+//       title: "car to buy",
+//       amount: 0.2,
+//     },
+//     {
+//       id: 3,
+//       date: new Date(2026, 9, 24),
+//       title: "biz income",
+//       amount: 20,
+//     },
+//     {
+//       id: 4,
+//       date: new Date(2024, 9, 24),
+//       title: "rented income",
+//       amount: 2,
+//     },
+//   ];
+
+//   return (
+//     <div className="App">
+//       <h1>Expense Tracking project</h1>
+//       <Expenses items={expenses} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
 
-import Expenses from "./components/Expenses/Expenses";
-
-// function App() {
-//using arrow functions - alternative function syntax
-const App = () => {
-  const expenses = [
-    {
-      id: 1,
-      date: new Date(2025, 5, 3),
-      title: "house to buy",
-      amount: 1.5,
-    },
-    {
-      id: 2,
-      date: new Date(2023, 6, 24),
-      title: "car to buy",
-      amount: 0.2,
-    },
-    {
-      id: 3,
-      date: new Date(2026, 9, 24),
-      title: "biz income",
-      amount: 20,
-    },
-    {
-      id: 4,
-      date: new Date(2024, 9, 24),
-      title: "rented income",
-      amount: 2,
-    },
-  ];
-
+function App() {
   return (
-    <div className="App">
-      <h1>Expense Tracking project</h1>
-      <Expenses items={expenses} />
+    <div>
+      <h1>App rendered</h1>
+      <p>this is also visible!</p>
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,40 @@
+// import React from "react";
+
+// import Card from "../UI/Card";
+// import "./ExpenseItem.css";
+// import ExpenseDate from "./ExpenseDate";
+
+// // function ExpenseItem(props) {
+// const ExpenseItem = (props) => {
+//   return (
+//     <Card className="expense-item">
+//       <ExpenseDate date={props.date} />
+//       <div className="expense-item__description">
+//         <h2>{props.title}</h2>
+//         <div className="expense-item__price ">Rs.{props.amount} Cr.</div>
+//       </div>
+//     </Card>
+//   );
+// };
+
+// export default ExpenseItem;
+
 import React from "react";
-
-import Card from "../UI/Card";
 import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
 
-// function ExpenseItem(props) {
-const ExpenseItem = (props) => {
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 4, 30);
+  const expenseTitle = "Car buying";
+  const expenseAmount = 20;
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price ">Rs.{props.amount} Cr.</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">Rs.{expenseAmount}L</div>
       </div>
-    </Card>
+    </div>
   );
-};
+}
 
 export default ExpenseItem;
