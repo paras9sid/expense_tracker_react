f693dae946284ac5123fad883b49e24e42c3a14f
paras9sid <sidharthjainp9@gmail.com>
2023-04-05 14:24:43 +0530

    Working with STATE

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
@@ -6,19 +6,23 @@ import ExpenseDate from "./ExpenseDate";
 
 // function ExpenseItem(props) {
 const ExpenseItem = (props) => {
+  //hooks starts iwth use , can be written in function only
+  const [title, setTitle] = useState(props.title);
+
   //below arrow function can be written as --> function clickHandle(){}
   const clickHandler = () => {
-    console.log("clicked!!");
+    setTitle("Updated!!");
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price ">Rs.{props.amount} Cr.</div>
       </div>
-      {/* button clciked and above title changed - passed via props */}
+      {/* todo-button clicked and above title changed - passed via props */}
       {/* adding event listener in button - listener starts with 'on' and wants as a function as a values */}
       {/* <button onClick={() => console.log("clicked")}>Change Title</button>--> will print clicked in console as per the stmt in arrow function in listener */}
 
