90fb2d02ad5af5d3a1adef0fc4a4a9127521175f
paras9sid <sidharthjainp9@gmail.com>
2023-04-12 16:34:38 +0530

    outputting dymanic content - 3 ways to do with conditional stmt

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,6 +15,19 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>No Expenses found!</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -22,14 +35,34 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No Expenses found!</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
+
+        {/* above ternary expressions can be used with && operator also */}
+        {/* 
+        {filteredExpenses.length === 0 && <p>No Expenses found!</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+
+        {/* 3rd way - decalring variable at top and put conditions on it */}
+        {expensesContent}
       </Card>
     </div>
   );
